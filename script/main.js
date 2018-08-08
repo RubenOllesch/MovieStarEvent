81ff69a43c3473ed42776bc604b3dd63a88c99d0
RubenOllesch <42176443+RubenOllesch@users.noreply.github.com>
2018-08-08 16:37:06 +0200

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,9 +1,44 @@
+'use strict';
 chayns.ready.then(function () {
-    /*
     var userName = "No user found. Log in to display your name here.";
     if (chayns.env.user.isAuthenticated) {
         userName = chayns.env.user.name;
-        document.getElementById("titleQuestion").innerHTML = userName + ', triff Tom Cruise!';
+        var userNameArray = userName.split(" ");
+        var userFirstName = userNameArray[0];
+        document.getElementById("titleQuestion").innerHTML = userFirstName + ', triff Tom Cruise!';
     }
-    */
 });
+
+function showRoute() {
+    var destination = 'offsite+Oldenkottplatz+Ahaus';
+    //Default location
+    var origin = '0,0';
+    
+    chayns.getGeoLocation()
+    .then(function(result) {
+        origin = getOrigin(JSON.stringify(result));
+        console.log(origin);
+    })
+    .then(function(origin) {
+        var mapURL = 'https://www.google.com/maps/embed/v1/directions?origin=' + origin + '&destination=' + destination + '&avoid=tolls|highways';
+        document.getElementById('cinemyMap').src = mapURL;
+    })
+    .catch((error) => {
+        console.log(error)
+    });
+}
+
+function getOrigin(locationDataString) {
+    //Removes braces
+    var locationDataString = locationDataString.replace('{','');
+    var locationDataString = locationDataString.replace('}','');
+    //Splits in lat, long, speed
+    var locationData = locationDataString.split(',');
+    //Removes indentifiers; keeps data (numbers)
+    for (let i = 0; i < locationData.length; i++) {
+        locationData[i] = locationData[i].split(':')[1];      
+    }
+    //Build new String
+    var origin = locationData[0] + ',' + locationData[1];
+    return origin;
+}
