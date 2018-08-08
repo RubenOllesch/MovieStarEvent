626ca9717f712096bd1a478b4f03edea70d7eeb5
RubenOllesch <42176443+RubenOllesch@users.noreply.github.com>
2018-08-08 17:37:44 +0200

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,4 +1,5 @@
 'use strict';
+
 chayns.ready.then(function () {
     var userName = "No user found. Log in to display your name here.";
     if (chayns.env.user.isAuthenticated) {
@@ -7,7 +8,29 @@ chayns.ready.then(function () {
         var userFirstName = userNameArray[0];
         document.getElementById("titleQuestion").innerHTML = userFirstName + ', triff Tom Cruise!';
     }
+
+    if(localStorage.getItem('hasEntered') === 'true') {
+        document.getElementById('contestField').innerHTML == 'wir freuen uns auf dich...';
+    }
 });
+
+function getTicketDialog() {
+    chayns.dialog.input({
+        title: 'Jetzt Ticket sichern',
+        message: 'Gebe ein Kommentar ab, um gewinnen zu können:',
+        placeholderText: 'Kommentar',
+        buttons: [{
+            text: 'Abschicken',
+            buttonType: 1
+        },{
+            text: 'Abbrechen',
+            buttonType: 0
+        }]
+       })
+       .then(function (data) {
+            commentHandler(JSON.stringify(data));
+       });
+}
 
 function showRoute() {
     var destination = 'offsite+Oldenkottplatz+Ahaus';
@@ -16,7 +39,8 @@ function showRoute() {
     
     chayns.getGeoLocation()
     .then(function(result) {
-        origin = getOrigin(JSON.stringify(result));
+        var locationData = JSONStringToArray(JSON.stringify(result));
+        origin = locationData[0] + ',' + locationData[1];
         console.log(origin);
     })
     .then(function(origin) {
@@ -28,17 +52,35 @@ function showRoute() {
     });
 }
 
-function getOrigin(locationDataString) {
-    //Removes braces
-    var locationDataString = locationDataString.replace('{','');
-    var locationDataString = locationDataString.replace('}','');
-    //Splits in lat, long, speed
-    var locationData = locationDataString.split(',');
-    //Removes indentifiers; keeps data (numbers)
-    for (let i = 0; i < locationData.length; i++) {
-        locationData[i] = locationData[i].split(':')[1];      
+function commentHandler(dialogDataString) {
+    var dialogData = JSONStringToArray(dialogDataString);
+    if (dialogData[0] === '1') {
+        var comment = dialogData[1];
+        sessionStorage.setItem('userComment', comment);
+        if (chayns.env.user.isAuthenticated) {
+            var JSONMessage = { userID : chayns.env.user.id, comment : sessionStorage.getItem('userComment')};
+            console.log(JSONMessage);
+            chayns.intercom.sendMessageToPage(JSONMessage)
+            .then(function(data){            
+                if(data.status == 200)
+                    var hasEntered = 'true';
+                    localStorage.setItem('hasEntered', hasEntered);
+                    chayns.dialog.alert(userFirstName, ', danke für dein Kommentar');
+            });
+        }
     }
-    //Build new String
-    var origin = locationData[0] + ',' + locationData[1];
-    return origin;
+    
+}
+
+function JSONStringToArray(dataString) {
+    //Removes braces
+    var dataString = dataString.replace('{','');
+    var dataString = dataString.replace('}','');
+    //Atomizes String into array
+    var data = dataString.split(',');
+    //Keeps data; discards identifiers
+    for (let i = 0; i < data.length; i++) {
+        data[i] = data[i].split(':')[1];      
+    }
+    return data;
 }
